06a72d8b1c1d36946d0ee888b8767cd6720861b3
roshanabdullah <abdullahroshan55@gmil.com>
2022-05-28 22:00:52 +0500

    mappingconcept

---
+++ map concept/src/components/App.jsx
@@ -0,0 +1,23 @@
+import React from "react";
+import Entry from "./Entry";
+import emojipedia from "../emojipedia";
+
+function App() {
+  return (
+    <div>
+      <h1>
+        <span>emojipedia</span>
+      </h1>
+
+      <dl className="dictionary">
+        {emojipedia.map((emo) => {
+          return (
+            <Entry emoji={emo.emoji} name={emo.name} meaning={emo.meaning} />
+          );
+        })}
+      </dl>
+    </div>
+  );
+}
+
+export default App;
